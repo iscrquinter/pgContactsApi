267db111b38aaa75f33bdef11eb4d45ab9fd3c00
Ricardo Quintero <iscrquinter@hotmail.com>
2017-09-10 18:18:16 -0600

    Agregando Contacts App...

---
+++ typings/cordova-typings.d.ts
@@ -0,0 +1,11 @@
+
+/// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Dialogs.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\FileTransfer.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Globalization.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\InAppBrowser.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Media.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\NetworkInformation.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Splashscreen.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\StatusBar.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Vibration.d.ts"/>
